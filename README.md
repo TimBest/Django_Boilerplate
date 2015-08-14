ebb9e9b201e3a5ae3ded6a492fa3a7950c80a1ea
Timothy Best <besttimothy1@gmail.com>
2015-08-14 10:40:13 -0400

    got sitemap and robots working

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ or
 [.tar.gz](https://github.com/TimothyBest/Django_Boilerplate/tarball/master)
 
 Setup:
- - change mysite/ and mysite/mysite/ to the name of your project
- - in .gitignore change every occurrence of mysite in the line "\*mysite/mysite/media/" to the name of your project
- - run "pip install requirements.txt"
- - add "export DEVELOPMENT=True" to ~/.profile
+ - Change every occirance of mysite to the name of your project
+ - Run "pip install requirements.txt"
+ - Add DEVELOPMENT=True to your environmental variables. For linux add "export DEVELOPMENT=True" to ~/.profile
  - set up a Postgres database (https://www.digitalocean.com/community/tutorials/how-to-set-up-django-with-postgres-nginx-and-gunicorn-on-centos-7)
+ - add your domain name to the sites model
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,19 +1,20 @@
 from django.conf import settings
 from django.conf.urls import include, patterns, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.sitemaps.views import sitemap
 
+from mysite.sitemap import StaticViewSitemap
+
+
 sitemaps = {
-    # List of apps sitemaps goes here
+    'static': StaticViewSitemap(),
 }
 
 admin.autodiscover()
 
-urlpatterns = staticfiles_urlpatterns()
-
-
-urlpatterns += patterns('',
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     url(r'^robots.txt$', include('robots.urls')),
@@ -25,8 +26,7 @@ urlpatterns += patterns('utils.views',
 )
 
 if settings.DEVELOPMENT:
-    urlpatterns += patterns('',
-        # TODO/WARNING: media url is hard coded in auto complete templates
-        url(r'^media/(?P<path>.*)$','django.views.static.serve',
-            {'document_root':  getattr(settings, 'MEDIA_ROOT', '/media')}),
-    )
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
